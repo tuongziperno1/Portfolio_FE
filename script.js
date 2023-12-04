efad4c43371b32fe632f49913676277041d701c2
tuongziperno1 <tantuong456456@gmail.com>
2023-12-04 09:10:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//=========================typing animation=========================
+var typed = new Typed(".typing", {
+    String: ["", "Web Designer", "Web Developer", "Graphic Designer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
